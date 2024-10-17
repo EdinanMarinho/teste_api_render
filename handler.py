95c84b1ca26c33a3b12eb866eded621678e59f6d
EdinanMarinho <61295729+EdinanMarinho@users.noreply.github.com>
2024-10-17 09:11:03 -0300

    Alterado port -> 5000

--- handler.py
+++ handler.py
@@ -46,6 +46,6 @@ def rossmann_predict():
         
 
 if __name__ == '__main__':
-    port = os.environ.get('PORT', 10000 )
-    app.run( host='0.0.0.0',port=port)
+    port = os.environ.get('PORT', 5000 )
+    app.run( host='0.0.0.0', port=port)
 
